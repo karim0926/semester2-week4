63b858cc419166cc094c57b0bbdfb3a9d46c9e75
karim0926 <rzvh0240@leeds.ac.uk>
2026-03-01 23:01:31 +0000

    Update student information and improve comments

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,19 +1,31 @@
-
 /*
-Name:
-Student ID:
+Name: Karim Ozoor
+Student ID: 201953003
 */
 
 #include <stdio.h>
 #include <string.h>
 
-int main( int argc, char **argv ) {
+int main(int argc, char **argv) {
 
-    // define appropriate data to hold your answer
+    // Buffer large enough for maximum possible output
+    // Max 10 arguments × 4 chars each = 40
+    // Plus 9 dashes = 9
+    // Plus null terminator = 1
+    char buffer[60] = "";
 
-    // process the command-line data using appropriate string functions
+    // Process command-line arguments
+    for (int i = 1; i < argc; i++) {
 
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+        strcat(buffer, argv[i]);
+
+        // Add dash between arguments (but not after last one)
+        if (i < argc - 1) {
+            strcat(buffer, "-");
+        }
+    }
+
+    printf("%s\n", buffer);  // use only this single print statement
 
     return 0;
 }
