3c6439d20c062d65ec994c395659930cdfe749f9
karim0926 <rzvh0240@leeds.ac.uk>
2026-04-04 13:06:07 +0000

    new

--- EvaluateExpression.asm
+++ EvaluateExpression.asm
@@ -1,32 +1,25 @@
-// Evaluate z = 17 + x - 3y
+// Compute z = 17 + x - 3y
 // Input:  R0 = x, R1 = y
 // Output: R2 = z
 // R0 and R1 must not be modified.
 
 @R1
 D=M
-@Y
-M=D
+@TEMP
+M=D            // TEMP = y
 
-@Y
+@R1
 D=M
-@Y2
-M=D+M          // Y2 = 2y
-
-@Y2
-D=M
-@Y
+D=D+M          // D = 2y
+@TEMP
 D=D+M          // D = 3y
-
 @THREEY
 M=D
 
 @17
-D=A
-
+D=A            // D = 17
 @R0
 D=D+M          // D = 17 + x
-
 @THREEY
 D=D-M          // D = 17 + x - 3y
 
